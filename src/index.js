993c25963021ab7d9da8a09998c9f8f8d2ade741
Sourabh Grover <sourabhgrover29@gmail.com>
2020-01-16 17:59:42 +0530

    Added envioremnt variable file and move Firebase config to .env file

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import App from "./components/App.js";
 const firebase = require("firebase");
 require("firebase/firestore");
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyC_2jCs-JmuBPim6DNH4btWPeuv6vsqGjQ",
-  authDomain: "mynotes-32095.firebaseapp.com",
-  databaseURL: "https://mynotes-32095.firebaseio.com",
-  projectId: "mynotes-32095",
-  storageBucket: "mynotes-32095.appspot.com",
-  messagingSenderId: "782579105978",
-  appId: "1:782579105978:web:1cc31014c48faeaee54283"
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
