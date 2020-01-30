5697d18dd1a9c9fa2c3ccb80e5085f2bc8834036
Sourabh Grover <sourabhgrover29@gmail.com>
2020-01-30 23:43:51 +0530

    List Side Bar Item Component In SideBar Component

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -15,27 +15,52 @@ class SideBar extends React.Component {
     }
     render() {
         const { classes, notes, selectedNoteIndex } = this.props;
-        return (
-            <div className={classes.sidebarContainer}>
-                <Button
-                    onClick={this.newNoteBtnClick}
-                    className={classes.newNoteBtn}>
-                    {this.state.addingNote ? 'Cancel' : 'New Note'}
-                </Button>
-                {
-                    this.state.addingNote ?
-                        <div>
-                            <input
-                                className={classes.newNoteInput}
-                                type="text" placeholder="Enter Note Title" onKeyUp={(e) => this.updateTitle(e.target.value)}>
-                            </input>
-                            <Button className={classes.newNoteSubmitButton} onClick={this.submitNote}>Submit</Button>
-                        </div>
-                        : null
+        if (notes) {
+            return (
+                <div className={classes.sidebarContainer}>
+                    <Button
+                        onClick={this.newNoteBtnClick}
+                        className={classes.newNoteBtn}>
+                        {this.state.addingNote ? 'Cancel' : 'New Note'}
+                    </Button>
+                    {
+                        this.state.addingNote ?
+                            <div>
+                                <input
+                                    className={classes.newNoteInput}
+                                    type="text" placeholder="Enter Note Title" onKeyUp={(e) => this.updateTitle(e.target.value)}>
+                                </input>
+                                <Button className={classes.newNoteSubmitButton} onClick={this.submitNote}>Submit</Button>
+                            </div>
+                            : null
+                    }
 
-                }
-            </div>
-        );
+
+                    <List>
+                        {
+                            notes.map(
+                                (_note, _index) => {
+                                    console.log(_note);
+                                    return (<div key={_index}>
+                                        <SidebarItems
+                                            _note={_note}
+                                            _index={_index}
+                                            selectedNoteIndex={selectedNoteIndex}
+                                            selectNote={this.selectNote}
+                                            deleteNote={this.deleteNote}
+                                        />
+                                    </div>);
+                                }
+                            )
+                        }
+                    </List>
+                    <Divider></Divider>
+
+                </div >
+            );
+        } else {
+            return (<div>Loading...</div>);
+        }
     }
 
     newNoteBtnClick = () => {
@@ -48,6 +73,8 @@ class SideBar extends React.Component {
         console.log(this.state);
         console.log("submit clikced");
     }
+    deleteNote = () => console.log("Delete Note");
+    selectNote = () => console.log("selectNote");
 }
 
 export default withStyles(style)(SideBar);
