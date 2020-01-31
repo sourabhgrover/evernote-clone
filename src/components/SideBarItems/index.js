0332fee2d440eaf65c1d6954459c483f66afb06c
Sourabh Grover <sourabhgrover29@gmail.com>
2020-01-31 20:31:35 +0530

    Write Boiler Plate for SideBar Item Component

--- src/components/SideBarItems/index.js
+++ src/components/SideBarItems/index.js
@@ -10,8 +10,41 @@ import { removeHTMLTags } from '../../helpers';
 class SideBarItems extends React.Component {
 
     render() {
-        return "Side Bar Items Component";
+        const { _index, _note, classes, selectedNoteIndex } = this.props;
+        return (
+            <div key={_index}>
+                <ListItem
+                    className={classes.listItem}
+                    selected={_index === selectedNoteIndex}
+                    alignItems='flex-start'
+                >
+                    <div
+                        className={classes.textSection}
+                        onClick={() => this.selectNote(_note, _index)}
+                    >
+                        <ListItemText primary={_note.title}
+                            secondary={removeHTMLTags(_note.body.substring(0, 30)) + '...'}
+                        >
+                        </ListItemText>
+                    </div>
+                    <DeleteIcon
+                        onClick={() => this.deleteNote(_note)}
+                        className={classes.deletIcon}
+                    >
+                    </DeleteIcon>
+
+                </ListItem>
+            </div>
+        );
+    }
+
+    selectNote = (note, index) => {
+        this.props.selectNote();
+    }
+    deleteNote = (note) => {
+        if (window.confirm(`Are you sure you want to delete ${note.title}`))
+            this.props.deleteNote()
     }
 }
 
-export default SideBarItems;
+export default withStyles(style)(SideBarItems);
