4335fd719c3cc477488400864c7f425016df5d8d
sourabh grover <sourabhgrover29@gmail.com>
2020-08-13 22:23:34 +0530

    Nav bar html

--- src/components/LeftNavBar.js
+++ src/components/LeftNavBar.js
@@ -2,7 +2,38 @@ import React from "react";
 
 const LeftNavBar = () =>{
     return(
-            <nav style={{border:"2px solid black"}} className="col-2">Nav bar thi is s</nav>
+            // <nav >
+            // <ul>
+            //     <li></li>
+            // </ul>
+            // </nav>
+
+<nav id="sidebar" class="active" style={{border:"2px solid black"}} className="col-2">
+<h1><a href="index.html" class="logo">M.</a></h1>
+<ul class="list-unstyled components mb-5">
+<li class="active">
+<a href="#"><span class="fa fa-home"></span> Home</a>
+</li>
+<li>
+<a href="#"><span class="fa fa-user"></span> About</a>
+</li>
+<li>
+<a href="#"><span class="fa fa-sticky-note"></span> Blog</a>
+</li>
+<li>
+<a href="#"><span class="fa fa-cogs"></span> Services</a>
+</li>
+<li>
+<a href="#"><span class="fa fa-paper-plane"></span> Contacts</a>
+</li>
+</ul>
+
+<div class="footer">
+<p>
+      Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+    </p>
+</div>
+</nav>
        
     );
     }
